b9d62faa58df16c60c820dcb09ca52f2b1293f32
KeiTeshima <142950238+KeiTeshima@users.noreply.github.com>
2023-10-10 07:41:27 +0900

    Update presentation.js

--- presentation.js
+++ presentation.js
@@ -2,16 +2,16 @@
 // 1行目に記載している 'use strict' は削除しないでください
 
 let element = document.getElementById("text");
-element.value = "従業員番号を入力してください";
+element.value = "従業員番号を入力";
 
-function gators(){
-    document.getElementsByClassName("bluebox")[0].innerText = "入会ありがとうございました";
+function join() {
+    document.getElementsByClassName("thankyoubox")[0].innerText = "入会ありがとうございました";
 }
 
-const target = document.getElementsByClassName("gator_button")[0];
-target.addEventListener("click", gators);
+const target = document.getElementsByClassName("join_button")[0];
+target.addEventListener("click", join);
 
-const images = ["img/01.png","img/02.png","img/03.png","img/04.png"];
+const images = ["img/01.png", "img/02.png", "img/03.png", "img/04.png"];
 const slideImg = document.getElementById("slide_img");
 const prev = document.getElementById("prev");
 const page = document.getElementById("page");
@@ -20,16 +20,16 @@ let counter = 0;
 
 page.textContent = `1/${images.length}`;
 
-next.addEventListener("click", function() {
-    if(counter + 1 < images.length) {
+next.addEventListener("click", function () {
+    if (counter + 1 < images.length) {
         counter++;
         slideImg.src = images[counter];
     }
     page.textContent = `${counter + 1}/${images.length}`;
 });
 
-prev.addEventListener("click", function() {
-    if(counter > 0) {
+prev.addEventListener("click", function () {
+    if (counter > 0) {
         counter--;
         slideImg.src = images[counter];
     }
